4441d25a8bedaaca0415dd3405813377eea8cd8f
Aasu <aasu@ordertrainings.com>
2019-03-09 13:29:44 +0530

    Basic form validation

--- add.php
+++ add.php
@@ -1,9 +1,31 @@
 <?php 
 
 	if(isset($_POST['submit'])){
-		echo $_POST['email'];
-		echo $_POST['title'];
-		echo $_POST['ingredients'];
+		/* echo htmlspecialchars($_POST['email']);
+		echo htmlspecialchars($_POST['title']);
+		echo htmlspecialchars($_POST['ingredients']); */
+
+		// check email
+		if (empty($_POST['email'])) {
+			echo "Email is required <br />";
+		}else{
+			echo htmlspecialchars($_POST['email']);
+		}
+
+		// check title
+		if(empty($_POST['title'])){
+			echo "Title is required <br />";
+		}else{
+			echo htmlspecialchars($_POST['title']);
+		}
+
+		// check ingredients
+		if (empty($_POST['ingredients'])) {
+			echo "At least one ingredient is required";
+		}else{
+			echo htmlspecialchars($_POST['ingredients']);
+		}
+		// end of check
 	}
 
  ?>
--- templates/header.php
+++ templates/header.php
@@ -1,27 +1,27 @@
 <head>
- 	<title>Pizza</title>
- 	<!-- Compiled and minified CSS -->
+    <title>Pizza</title>
+    <!-- Compiled and minified CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
     <style type="text/css">
-    	.brand{
-    		background: #cbb09c !important;
-    	}
-    	.brand-text{
-    		color: #cbb09c !important;
-    	}
-    	form{
-    		max-width: 560px;
-    		margin: 20px auto;
-    		padding: 20px;
-    	}
+        .brand{
+            background: #cbb09c !important;
+        }
+        .brand-text{
+            color: #cbb09c !important;
+        }
+        form{
+            max-width: 560px;
+            margin: 20px auto;
+            padding: 20px;
+        }
     </style>
  </head>
  <body class="grey lighten-4">
- 	<nav class="white z-depth-0">
- 		<div class="container">
- 			<a href="#" class="brand-logo brand-text">Pizza</a>
- 			<ul id="nav-mobile" class="right hide-on-small-and-down">
- 				<li><a href="#" class="btn brand z-depth-0">Add a Pizza</a></li>
- 			</ul>
- 		</div>
- 	</nav>
+    <nav class="white z-depth-0">
+        <div class="container">
+            <a href="#" class="brand-logo brand-text">Pizza</a>
+            <ul id="nav-mobile" class="right hide-on-small-and-down">
+                <li><a href="#" class="btn brand z-depth-0">Add a Pizza</a></li>
+            </ul>
+        </div>
+    </nav>
