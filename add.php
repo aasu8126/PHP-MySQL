dc6b25867446c6688abf0a4b5ae1994e2a99af6a
Aasu <aasu@ordertrainings.com>
2019-03-09 14:10:51 +0530

    Filters and more validation

--- add.php
+++ add.php
@@ -9,21 +9,33 @@
 		if (empty($_POST['email'])) {
 			echo "Email is required <br />";
 		}else{
-			echo htmlspecialchars($_POST['email']);
+			// echo htmlspecialchars($_POST['email']);
+			$email = $_POST['email'];
+			if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+				echo 'email must be a valid email address <br />' ;
+			}
 		}
 
 		// check title
 		if(empty($_POST['title'])){
 			echo "Title is required <br />";
 		}else{
-			echo htmlspecialchars($_POST['title']);
+			// echo htmlspecialchars($_POST['title']);
+			$title = $_POST['title'];
+			if(!preg_match('/^[a-zA-Z\s]+$/', $title)){
+				echo "Title must be letters and spaces only <br />";
+			}
 		}
 
 		// check ingredients
 		if (empty($_POST['ingredients'])) {
 			echo "At least one ingredient is required";
 		}else{
-			echo htmlspecialchars($_POST['ingredients']);
+			// echo htmlspecialchars($_POST['ingredients']);
+			$ingredients = $_POST['ingredients'];
+			if (!preg_match('/^([a-zA-Z\s]+)(,\s*[a-zA-Z\s]*)*$/', $ingredients)) {
+				echo "Ingredients must be comma separated.";
+			}
 		}
 		// end of check
 	}
